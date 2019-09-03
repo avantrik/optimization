e081738a46b664625b8720d6b0116fa01ae4d052
tacfnt <Niyati200%>
2019-09-03 15:34:20 +0100

    Initial commit

---
+++ src/main/java/com/example/optimisation/OptimisationApplication.java
@@ -0,0 +1,13 @@
+package com.example.optimisation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimisationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OptimisationApplication.class, args);
+    }
+
+}
